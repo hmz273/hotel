515117e20b56dee477f5a9feb782ee0a59c40f67
hmz273 <73111873+hmz273@users.noreply.github.com>
2024-03-06 15:59:18 +0100

    booking form

--- app/compoment/Booking.tsx
+++ app/compoment/Booking.tsx
@@ -2,11 +2,59 @@ import React from 'react'
 
 const Booking = () => {
   return (
-    <section className="p-4 mx-auto my-8 max-w-screen-lg lg:mb-16 text-center bg-fuchsia-950 rounded-lg shadow dark:bg-gray-900 mt-4">
-          <h3 className="mb-2 text-2xl tracking-tight font-extrabold text-white dark:text-white">
+    <section className="p-4  mx-auto my-8 max-w-screen-lg lg:mb-16 text-center bg-fuchsia-950 rounded-lg shadow dark:bg-gray-900 mt-4">
+          <h3 className="mb-2 pt-4 text-2xl tracking-tight font-extrabold text-white dark:text-white">
             Booking Room
           </h3>
-        </section>
+
+          
+<div date-rangepicker className="grid grid-cols-1 items-center m-6 p-6">
+  
+  <div className="relative grid">
+    
+    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+        </svg>
+    </div>
+    <input name="start" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start"/>
+  </div>
+  <span className="mx-4 text-white">to</span>
+  
+  <div className="relative grid my-4">
+    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+        </svg>
+    </div>
+    <input name="end" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date end"/>
+</div>
+  <div className="relative grid my-4 ">
+    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+  </svg>
+  
+    </div>
+    <input data-input-counter-min="0" name="end" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Guests"/>
+</div>
+  <div className="relative grid my-4 ">
+    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+  </svg>
+  
+    </div>
+    <input data-input-counter-min="0" name="end" type="number" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Childrens"/>
+</div>
+  <div className="relative grid my-4">
+  <button type="button" className="mt-2 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Search</button>
+</div>
+</div>
+
+
+
+    </section>
   )
 }
 
