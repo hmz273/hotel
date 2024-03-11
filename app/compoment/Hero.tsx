f313a00eae1c73e76264db872131124c94e661f7
hmz273 <73111873+hmz273@users.noreply.github.com>
2024-03-11 20:09:04 +0000

    adding search form

--- app/compoment/Hero.tsx
+++ app/compoment/Hero.tsx
@@ -1,161 +1,114 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 
 const Hero = () => {
-  return (
-    <section id="Home">
-          <div
-            id="default-carousel"
-            className="mx-auto relative w-full"
-            data-carousel="slide"
-          >
-            {/* <!-- Carousel wrapper --> */}
-            <div className="relative overflow-hidden h-dvh">
-              {/* <!-- Item 1 --> */}
-              <div
-                className="hidden duration-700 ease-in-out"
-                data-carousel-item
-              >
-                <img
-                  src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg"
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  alt="..."
-                />
-              </div>
-              {/* <!-- Item 2 --> */}
-              <div
-                className="hidden duration-700 ease-in-out"
-                data-carousel-item
-              >
-                <img
-                  src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg"
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  alt="..."
-                />
-              </div>
-              {/* <!-- Item 3 --> */}
-              <div
-                className="hidden duration-700 ease-in-out"
-                data-carousel-item
-              >
-                <img
-                  src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg"
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  alt="..."
-                />
-              </div>
-              {/* <!-- Item 4 --> */}
-              <div
-                className="hidden duration-700 ease-in-out"
-                data-carousel-item
-              >
-                <img
-                  src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg"
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  alt="..."
-                />
-              </div>
-              {/* <!-- Item 5 --> */}
-              <div
-                className="hidden duration-700 ease-in-out"
-                data-carousel-item
-              >
-                <img
-                  src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg"
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                  alt="..."
-                />
-              </div>
-            </div>
-            {/* <!-- Slider indicators --> */}
-            <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="true"
-                aria-label="Slide 1"
-                data-carousel-slide-to="0"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 2"
-                data-carousel-slide-to="1"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 3"
-                data-carousel-slide-to="2"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 4"
-                data-carousel-slide-to="3"
-              ></button>
-              <button
-                type="button"
-                className="w-3 h-3 rounded-full"
-                aria-current="false"
-                aria-label="Slide 5"
-                data-carousel-slide-to="4"
-              ></button>
-            </div>
-            {/* <!-- Slider controls --> */}
-            <button
-              type="button"
-              className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-prev
-            >
-              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 1 1 5l4 4"
-                  />
-                </svg>
-                <span className="sr-only">Previous</span>
-              </span>
-            </button>
-            <button
-              type="button"
-              className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-              data-carousel-next
-            >
-              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg
-                  className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-                <span className="sr-only">Next</span>
-              </span>
-            </button>
-          </div>
-        </section>
-  )
-}
+  const [checkInDate, setCheckInDate] = useState('');
+  const [checkOutDate, setCheckOutDate] = useState('');
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
 
-export default Hero
+  const handleAdultsChange = (e) => {
+    setAdults(e.target.value);
+  };
+
+  const handleChildrenChange = (e) => {
+    setChildren(e.target.value);
+  };
+  const handleCheckInChange = (e) => {
+    setCheckInDate(e.target.value);
+  };
+
+  const handleCheckOutChange = (e) => {
+    setCheckOutDate(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to search available rooms and calculate total price
+    console.log('Check-in Date:', checkInDate, 'Check-out Date:', checkOutDate, 'adults:', adults, 'children:', children);
+  };
+
+  return (
+    <section id="Home" className="static z-0">
+      <div id="default-carousel" className="mx-auto relative w-full" data-carousel="slide">
+        <div className="relative xs:h-56 md:h-dvh overflow-hidden">
+          {/* <!-- Item 1 --> */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+          </div>
+          {/* <!-- Item 2 --> */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+          </div>
+          {/* <!-- Item 3 --> */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+          </div>
+          {/* <!-- Item 4 --> */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+          </div>
+          {/* <!-- Item 5 --> */}
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+              <img src="https://pics.uncubus.tech/images/11242/upload/1500X770/5e7caa18ac4a59cfbca0a24fc5cffe74.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+          </div>
+        </div>
+      </div>
+
+      <div className="md:z-30 absolute bottom-6 left-6 max-w-md mx-auto bg-purple-100 p-8 rounded-xl">
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+          <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-800 md:text-2xl dark:text-white">Price Match, Made Simple</h1>
+          <label htmlFor="checkInDate" className="block text-sm font-medium text-gray-700">Check-in Date:</label>
+          <input
+            type="date"
+            id="checkInDate"
+            value={checkInDate}
+            onChange={handleCheckInChange}
+            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 bg-purple-50 focus:ring-purple-500"
+            required
+          />
+
+          <label htmlFor="checkOutDate" className="block text-sm font-medium text-gray-700">Check-out Date:</label>
+          <input
+            type="date"
+            id="checkOutDate"
+            value={checkOutDate}
+            onChange={handleCheckOutChange}
+            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 bg-purple-50 focus:ring-purple-500"
+            required
+          />
+          <label htmlFor="adults" className="block text-sm font-medium text-gray-700">
+            Adults:
+          </label>
+          <input
+            type="number"
+            id="adults"
+            name="adults"
+            min="1"
+            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 bg-purple-50 focus:ring-purple-500"
+            value={adults}
+            onChange={handleAdultsChange}
+            required
+          />
+          <label htmlFor="children" className="block text-sm font-medium text-gray-700">
+            Children:
+          </label>
+          <input
+            type="number"
+            id="children"
+            name="children"
+            min="0"
+            className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 bg-purple-50 focus:ring-purple-500"
+            value={children}
+            onChange={handleChildrenChange}
+          />
+          <button type="submit" className="px-6 py-3 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none focus:bg-purple-700">
+            Search Rooms
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- app/compoment/RoomModel.jsx
+++ app/compoment/RoomModel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const RoomModel = ({room, trrooms}) => {
-    console.log(room, trrooms);
+
   return (
     <div
                   id="extralarge-modal"
