d1eeb1bc48ffc73941676e3e557ac39ca4bff24c
hmz273 <73111873+hmz273@users.noreply.github.com>
2024-03-14 00:30:51 +0000

    fixing accordin opens

--- app/compoment/AccordionItem.tsx
+++ app/compoment/AccordionItem.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AccordionItem = ({ title, content, index, trdata }) => {
-  const accordionId = `accordion-collapse-${index}`;
+  const [activeIndex, setActiveIndex] = useState(null);
 
+  const toggleAccordion = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
   return (
-    <div id={accordionId} data-accordion="collapse">
-      <h2 id={`accordion-heading-${index}`}>
+    <div key={index} id={`accordion-collapse-${index}`} data-accordion="collapse">
+      <h2 id={`accordion-collapse-heading-${index}`}>
         <button
           type="button"
           className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-          data-accordion-target={`#${accordionId}-body`}
-          aria-expanded="true"
-          aria-controls={`${accordionId}-body`}
+          onClick={() => toggleAccordion(index)}
+              aria-expanded={activeIndex === index}
+              aria-controls={`accordion-collapse-body-${index}`}
         >
           <span>{title}</span>
           <svg
@@ -33,9 +36,11 @@ const AccordionItem = ({ title, content, index, trdata }) => {
         </button>
       </h2>
       <div
-        id={`${accordionId}-body`}
-        className="hidden"
-        aria-labelledby={`accordion-heading-${index}`}
+        id={`accordion-collapse-body-${index}`}
+        className={`p-5 border border-b-0 border-gray-200 dark:border-gray-700 ${
+          activeIndex === index ? '' : 'hidden'
+        }`}
+        aria-labelledby={`accordion-collapse-heading-${index}`}
       >
         <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
           {content.map((item, idx) => (
